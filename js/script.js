8e58949fd734b86e4db1b9f1a011a69a81e75fb0
Tizziano <tizzirodri@gmail.com>
2025-10-14 19:51:57 -0300

    Primera implementacion javaScript

---
+++ js/script.js
@@ -0,0 +1,20 @@
+document.addEventListener( 'DOMContentLoaded' , () => {
+    const hamburgerButton = document.querySelector('.hamburger-menu');
+    const mainNav = document.getElementById('main-nav');
+  
+    hamburgerButton.addEventListener( 'click' , () => {
+      const isExpanded = hamburgerButton
+        .getAttribute( 'aria-expanded' ) === 'true' ;
+      hamburgerButton.setAttribute( 'aria-expanded' , !isExpanded );
+      mainNav.classList.toggle( 'nav-open' ) ; 
+    } ) ;
+    
+    document.addEventListener( 'click' , (event) => {
+      if( !mainNav.contains( event.target ) &&
+          !hamburgerButton.contains( event.target ) &&
+          mainNav.classList.contains( 'nav-open' ) ) {
+        hamburgerButton.setAttribute( 'aria-expanded' , 'false' ) ;
+        mainNav.classList.remove( 'nav-open' ) ;
+      }
+    } ) ;
+  } ) ;
